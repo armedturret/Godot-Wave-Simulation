92e6ec011e1bc993286d4b78055a8826b9010174
armedturret <rjwhitenh@outlook.com>
2026-03-18 11:16:54 -0400

    Added noise to the waves

--- scripts/ocean_generator.gd
+++ scripts/ocean_generator.gd
@@ -14,27 +14,27 @@ var generate_ocean_tool_button: Callable = regenerate_ocean_params
 
 
 func regenerate_ocean_params() -> void:
-	var ocean_material := get_surface_override_material(0) as ShaderMaterial
+    var ocean_material := get_surface_override_material(0) as ShaderMaterial
 
-	if ocean_material == null:
-		return
+    if ocean_material == null:
+        return
 
-	var amplitudes : Array[float] = []
-	var wave_dirs : Array[Vector2] = []
-	var wave_lens : Array[float] = []
-	var phases : Array[float] = []
-	for i in range(wave_count):
-		# If kA > 1 causes looping waves
-		# Cap for now
-		var wave_len := randf_range(min_wavelen, max_wavelen)
-		var wave_dir := randf_range(0, 2.0 * PI)
-		var amp := randf_range(min_amplitude, max_amplitude)
-		amplitudes.append(amp)
-		wave_dirs.append(Vector2(cos(wave_dir), sin(wave_dir)))
-		wave_lens.append(wave_len)
-		phases.append(randf_range(0, 2.0 * PI))
+    var amplitudes : Array[float] = []
+    var wave_dirs : Array[Vector2] = []
+    var wave_lens : Array[float] = []
+    var phases : Array[float] = []
+    for i in range(wave_count):
+        # If kA > 1 causes looping waves
+        # Cap for now
+        var wave_len := randf_range(min_wavelen, max_wavelen)
+        var wave_dir := randf_range(0, 2.0 * PI)
+        var amp := randf_range(min_amplitude, max_amplitude)
+        amplitudes.append(amp)
+        wave_dirs.append(Vector2(cos(wave_dir), sin(wave_dir)))
+        wave_lens.append(wave_len)
+        phases.append(randf_range(0, 2.0 * PI))
 
-	ocean_material.set_shader_parameter("amp", amplitudes)
-	ocean_material.set_shader_parameter("wave_dirs", wave_dirs)
-	ocean_material.set_shader_parameter("wave_lens", wave_lens)
-	ocean_material.set_shader_parameter("phases", phases)
+    ocean_material.set_shader_parameter("amp", amplitudes)
+    ocean_material.set_shader_parameter("wave_dirs", wave_dirs)
+    ocean_material.set_shader_parameter("wave_lens", wave_lens)
+    ocean_material.set_shader_parameter("phases", phases)
